49fb639d18f3b42825277ab420e5578f12333850
ide user ide_cshukai <cshukai@gmail.com>
2012-05-29 04:44:33 +0000

    bcrImgPatient

--- LookUpTblClass.js
+++ LookUpTblClass.js
@@ -1,7 +1,7 @@
 
 // SPARQL queries for finding relationships between pid and data-type 
-var cancerType='';
-var dataTypeByDisease=[ "prefix tcga:<http://purl.org/tcga/core#>",
+function getDataTypeByDisease(cancerType){
+      var sparql_template= [ "prefix tcga:<http://purl.org/tcga/core#>",
                         "prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
                         "prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#>",
                         "prefix owl:<http://www.w3.org/2002//07/owl#>",                       
@@ -17,9 +17,47 @@ var dataTypeByDisease=[ "prefix tcga:<http://purl.org/tcga/core#>",
                            "?file tcga:disease-study ?d . ",
                            "?d rdfs:label "+"\'"+cancerType+"\'"+"."+"}"
                            ];
+                           
+        var sparql_query=sparql_template.join(" ");
+        var result={};
+        TCGA.hub.query(sparql_query,function(error,data){console.log(error);result=data;});
+        
+        //result is an array where every element is a type of biomedical data
+        // tofix
+        return(result);
+    }
 
-var dataTypeByDisease_query=dataTypeByDisease.join(" ");
 
+
+var  patientsOfImageData={};
+function  getPatientsInBcrImages(cancerType){
+          var sparql_template= [ "prefix tcga:<http://purl.org/tcga/core#>",
+                                 "prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+                                 "prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#>",
+                                 "prefix owl:<http://www.w3.org/2002//07/owl#>",                       
+                                 "prefix dc:<http://purl.org/dc/ele07/owl#>",
+                                 "prefix dc:<http://purl.org/dc/elements/1.1/>",
+                                 "prefix dcterms:<http://purl.org/dc/terms/>",
+                                 "prefix foaf:<http://xmlns.com/foaf/0.1/>",
+                                 "prefix fti:<http://franz.com/ns/allegrograph/2.2/textindex/>",
+                                 "prefix skos:<http://www.w3.org/2004/02/skos/core#>",
+                           "select distinct ?dataType ?name where { ",
+                           "?file tcga:url ?url .",
+                           "?file rdfs:label ?name .",
+                           "filter ( contains(?name, "+"\'"+".svs"+"\'"+") ) .",
+                           "?file tcga:data-type ?d.",
+                           "?d rdfs:label ?dataType.",
+                           "?file tcga:disease-study ?ds . ",
+                           "?ds rdfs:label "+"\'"+cancerType+"\'"+"."+"}"
+                           ];
+                           
+        var sparql_query=sparql_template.join(" ");
+       
+        TCGA.hub.query(sparql_query,function(error,data){console.log(error); patientsOfImageData=data;});
+        
+        //result is an array where every element is a type of biomedical data
+       
+    }
 
 
 // automating inheritance contruction
