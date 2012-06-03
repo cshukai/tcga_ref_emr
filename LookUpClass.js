d88912944aba8ab64f5837eedf0d92902a566cba
ide user ide_cshukai <cshukai@gmail.com>
2012-06-03 04:12:54 +0000

    clicOfLookUpTree

--- LookUpClass.js
+++ LookUpClass.js
@@ -15,6 +15,10 @@
  	 }
   	
     
+    function fillClinFields(lookUpTree,aDisease){
+    	
+    }
+    
     
     function fillFieldsGivenDataType(allDiseases,lookUpTree){
     	for(var i=0; i<allDiseases.length ; i++){
@@ -24,14 +28,14 @@
     		    if(lookUpTree[allDiseases[i]]['dataType'][j].match(/slide_images/)){
     		    	lookUpTree[allDiseases[i]]['slide_images']={};  		    	
     		    	lookUpTree[allDiseases[i]]['slide_images']['url']=new Array();
-    		    	lookUpTree[allDiseases[i]]['slide_images']['url']['pid']=new Array();
+    		    	lookUpTree[allDiseases[i]]['slide_images']['url']['tcga_barcode']=new Array();
     		    }
     		    
     		    
     		      if(lookUpTree[allDiseases[i]]['dataType'][j].match(/clin/)){
     		    	lookUpTree[allDiseases[i]]['clin']={};
     		    	lookUpTree[allDiseases[i]]['clin']['url']=new Array();
-    		    	lookUpTree[allDiseases[i]]['clin']['url']['pid']=new Array();
+    		    	
     		    }	
     		    
     		    	
