0c62d1a1709e643f3497f5015ecfbbf44ff5155c
ide user ide_cshukai <cshukai@gmail.com>
2012-06-06 22:17:50 +0000

    startTakingOffLookUpTree

--- DiseaseClass.js
+++ DiseaseClass.js
@@ -237,6 +237,38 @@ Disease.prototype.getPatientsInBcrImages=function(lookUpTree){
  
  
  
+ 
+   Disease.prototype.getDataTypes=function(){
+   	var that=this;
+   	var sparql_template= [ "prefix tcga:<http://purl.org/tcga/core#>",
+                        "prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+                        "prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#>",
+                        "prefix owl:<http://www.w3.org/2002//07/owl#>",                       
+                        "prefix dc:<http://purl.org/dc/ele07/owl#>",
+                        "prefix dc:<http://purl.org/dc/elements/1.1/>",
+                        "prefix dcterms:<http://purl.org/dc/terms/>",
+                        "prefix foaf:<http://xmlns.com/foaf/0.1/>",
+                        "prefix fti:<http://franz.com/ns/allegrograph/2.2/textindex/>",
+                        "prefix skos:<http://www.w3.org/2004/02/skos/core#>",
+                           "select distinct ?type where { ",
+                           "?file tcga:data-type ?t . ",
+                           "?t rdfs:label ?type . ",
+                           "?file tcga:disease-study ?d . ",
+                           "?d rdfs:label "+"\'"+this.diseaseName+"\'"+"."+"}"
+                           ];
+                           
+        var sparql_query=sparql_template.join(" ");                       // note :this refers to the windows
+        TCGA.hub.query(sparql_query,function(error,data){console.log(error);that.dataTypes= queryResultObj2Array(data,0);});
+   
+        
+        
+                 
+         
+       }
+ 
+ 
+ 
+ 
    Disease.prototype.getDataTypeByDisease=function(lookUpTree){
    	var that=this;
    	var sparql_template= [ "prefix tcga:<http://purl.org/tcga/core#>",
